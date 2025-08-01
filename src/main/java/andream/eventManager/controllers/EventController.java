d9cf9798ee0650d186b050662a5f6f4d6a4b116e
Andrea Maiolo <maiolo.m.2@gmail.com>
2025-08-01 12:42:36 +0200

    added getall and post for user and event

--- src/main/java/andream/eventManager/controllers/EventController.java
+++ src/main/java/andream/eventManager/controllers/EventController.java
@@ -1,8 +1,12 @@
 package andream.eventManager.controllers;
 
+import andream.eventManager.entities.Event;
 import andream.eventManager.services.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +14,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class EventController {
     @Autowired
     private EventService eventService;
+
+
+    @GetMapping
+    public Page<Event> findAll(@RequestParam(defaultValue = "0") int pageNumber,
+                               @RequestParam(defaultValue = "10") int pageSize,
+                               @RequestParam(defaultValue = "username") String sortBy
+    ) {
+        return this.eventService.findAll(pageNumber, pageSize, sortBy);
+    }
+
+//    @PostMapping
+//    @ResponseStatus(HttpStatus.CREATED)
+//    public EventRespDTO save(@RequestBody @Validated EventDTO payload, BindingResult validationResult) {
+//        if (validationResult.hasErrors()) {
+//            throw new BadRequestException("da fare con errore guisto");
+//        } else {
+//            Event newEvent = this.eventService.save(payload);
+//            return new EventRespDTO(newEvent.getId());
+//        }
+//    }
 }
--- src/main/java/andream/eventManager/controllers/UserController.java
+++ src/main/java/andream/eventManager/controllers/UserController.java
@@ -1,13 +1,40 @@
 package andream.eventManager.controllers;
 
+import andream.eventManager.entities.User;
+import andream.eventManager.exceptions.BadRequestException;
+import andream.eventManager.payloads.NewUserDTO;
+import andream.eventManager.payloads.UserRespDTO;
 import andream.eventManager.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserService userService;
+
+
+    @GetMapping
+    public Page<User> findAll(@RequestParam(defaultValue = "0") int pageNumber,
+                              @RequestParam(defaultValue = "10") int pageSize,
+                              @RequestParam(defaultValue = "username") String sortBy
+    ) {
+        return this.userService.findAll(pageNumber, pageSize, sortBy);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserRespDTO save(@RequestBody @Validated NewUserDTO payload, BindingResult validationResult) {
+        if (validationResult.hasErrors()) {
+            throw new BadRequestException("da fare con errore guisto");
+        } else {
+            User newU = this.userService.save(payload);
+            return new UserRespDTO(newU.getId());
+        }
+    }
 }
