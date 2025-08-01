2876bda6aaff1aba80c7da0bc98afecb0c853079
Andrea Maiolo <maiolo.m.2@gmail.com>
2025-08-01 10:36:39 +0200

    initial commit

---
+++ src/test/java/andream/eventManager/EventManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package andream.eventManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
