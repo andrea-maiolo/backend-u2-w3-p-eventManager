3ff34076e03ddb31c8680af59f237faeb04eed02
Andrea Maiolo <maiolo.m.2@gmail.com>
2025-08-01 15:47:35 +0200

    added modify method and delete

--- src/main/java/andream/eventManager/controllers/EventController.java
+++ src/main/java/andream/eventManager/controllers/EventController.java
@@ -2,7 +2,7 @@ package andream.eventManager.controllers;
 
 import andream.eventManager.entities.Event;
 import andream.eventManager.entities.User;
-import andream.eventManager.exceptions.BadRequestException;
+import andream.eventManager.exceptions.ValidationException;
 import andream.eventManager.payloads.EventDTO;
 import andream.eventManager.payloads.EventRespDTO;
 import andream.eventManager.services.EventService;
@@ -16,6 +16,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/events")
 public class EventController {
@@ -26,10 +28,10 @@ public class EventController {
 
     //serve a tutti gli utenti
     @GetMapping
-    @PreAuthorize("hasAuthority('USER')")
+    @PreAuthorize("isAuthenticated()")
     public Page<Event> findAll(@RequestParam(defaultValue = "0") int pageNumber,
                                @RequestParam(defaultValue = "10") int pageSize,
-                               @RequestParam(defaultValue = "username") String sortBy
+                               @RequestParam(defaultValue = "id") String sortBy
     ) {
         return this.eventService.findAll(pageNumber, pageSize, sortBy);
     }
@@ -41,17 +43,33 @@ public class EventController {
     public EventRespDTO save(@RequestBody @Validated EventDTO payload, @AuthenticationPrincipal User currentUser,
                              BindingResult validationResult) {
         if (validationResult.hasErrors()) {
-            throw new BadRequestException("da fare con errore guisto");
+            throw new ValidationException(validationResult.getFieldErrors()
+                    .stream()
+                    .map(fieldError -> fieldError.getDefaultMessage())
+                    .toList());
         } else {
             Event newEvent = this.eventService.save(payload, currentUser.getId());
             return new EventRespDTO(newEvent.getId());
         }
     }
 
-    //put su evento sempre organizer
+    //put su evento sempre organizer se e proprietario
+    @PutMapping("/{eventId}")
+    @PreAuthorize("hasAuthority('ORGANIZER')")
+    public Event updateEvent(@PathVariable UUID eventId,
+                             @Validated @RequestBody EventDTO payload,
+                             @AuthenticationPrincipal User currentUser
+    ) {
+        return this.eventService.findByIdAndUpdate(eventId, payload, currentUser);
+    }
+
     //delete su evento di organizer
-    //get by id sempre organizer
-    //all the above solo se si e il creatore dell evento
+    @DeleteMapping("/{eventId}")
+    @PreAuthorize("hasAuthority('ORGANIZER')")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void findAndDelete(@PathVariable UUID eventId) {
+        this.eventService.findByIdAndDelete(eventId);
+    }
 
     //prenotazione da parte di user
 }
--- src/main/java/andream/eventManager/services/EventService.java
+++ src/main/java/andream/eventManager/services/EventService.java
@@ -3,6 +3,7 @@ package andream.eventManager.services;
 import andream.eventManager.entities.Event;
 import andream.eventManager.entities.User;
 import andream.eventManager.exceptions.NotFoundException;
+import andream.eventManager.exceptions.UnAuthorizedException;
 import andream.eventManager.payloads.EventDTO;
 import andream.eventManager.repositories.EventRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,8 +42,13 @@ public class EventService {
     }
 
 
-    public Event findByIdAndUpdate(UUID eventId, EventDTO payload) {
+    public Event findByIdAndUpdate(UUID eventId, EventDTO payload, User currentUser) {
         Event found = this.findById(eventId);
+
+        if (found.getOrganizer() != currentUser) {
+            throw new UnAuthorizedException("you cannot modify events that you have not created");
+        }
+
         found.setTitle(payload.title());
         found.setDate(payload.date());
         found.setLocation(payload.location());
