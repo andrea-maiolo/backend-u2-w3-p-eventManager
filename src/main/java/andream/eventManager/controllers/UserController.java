972d028c3e40b29f1b553fd8177008894915bb82
Andrea Maiolo <maiolo.m.2@gmail.com>
2025-08-01 14:54:53 +0200

    removed post because u can register form auth controller

--- src/main/java/andream/eventManager/controllers/UserController.java
+++ src/main/java/andream/eventManager/controllers/UserController.java
@@ -1,16 +1,9 @@
 package andream.eventManager.controllers;
 
-import andream.eventManager.entities.User;
-import andream.eventManager.exceptions.BadRequestException;
-import andream.eventManager.payloads.NewUserDTO;
-import andream.eventManager.payloads.UserRespDTO;
 import andream.eventManager.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
@@ -18,22 +11,26 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping
-    public Page<User> findAll(@RequestParam(defaultValue = "0") int pageNumber,
-                              @RequestParam(defaultValue = "10") int pageSize,
-                              @RequestParam(defaultValue = "username") String sortBy
-    ) {
-        return this.userService.findAll(pageNumber, pageSize, sortBy);
-    }
+    //non serve
+//    @GetMapping
+//    public Page<User> findAll(@RequestParam(defaultValue = "0") int pageNumber,
+//                              @RequestParam(defaultValue = "10") int pageSize,
+//                              @RequestParam(defaultValue = "username") String sortBy
+//    ) {
+//        return this.userService.findAll(pageNumber, pageSize, sortBy);
+//    }
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public UserRespDTO save(@RequestBody @Validated NewUserDTO payload, BindingResult validationResult) {
-        if (validationResult.hasErrors()) {
-            throw new BadRequestException("da fare con errore guisto");
-        } else {
-            User newU = this.userService.save(payload);
-            return new UserRespDTO(newU.getId());
-        }
-    }
+    //in auth for free access
+//    @PostMapping
+//    @ResponseStatus(HttpStatus.CREATED)
+//    public UserRespDTO save(@RequestBody @Validated NewUserDTO payload, BindingResult validationResult) {
+//        if (validationResult.hasErrors()) {
+//            throw new BadRequestException("da fare con errore guisto");
+//        } else {
+//            User newU = this.userService.save(payload);
+//            return new UserRespDTO(newU.getId());
+//        }
+//    }
+
+
 }
