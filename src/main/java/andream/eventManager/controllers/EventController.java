61081d099b6fc3b1f216c74dd2b42f3e9df41b07
Andrea Maiolo <maiolo.m.2@gmail.com>
2025-08-01 14:55:18 +0200

    added get all for users and post for organizers

--- src/main/java/andream/eventManager/controllers/EventController.java
+++ src/main/java/andream/eventManager/controllers/EventController.java
@@ -1,22 +1,32 @@
 package andream.eventManager.controllers;
 
 import andream.eventManager.entities.Event;
+import andream.eventManager.entities.User;
+import andream.eventManager.exceptions.BadRequestException;
+import andream.eventManager.payloads.EventDTO;
+import andream.eventManager.payloads.EventRespDTO;
 import andream.eventManager.services.EventService;
+import andream.eventManager.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/events")
 public class EventController {
     @Autowired
     private EventService eventService;
+    @Autowired
+    private UserService userService;
 
-
+    //serve a tutti gli utenti
     @GetMapping
+    @PreAuthorize("hasAuthority('USER')")
     public Page<Event> findAll(@RequestParam(defaultValue = "0") int pageNumber,
                                @RequestParam(defaultValue = "10") int pageSize,
                                @RequestParam(defaultValue = "username") String sortBy
@@ -24,14 +34,24 @@ public class EventController {
         return this.eventService.findAll(pageNumber, pageSize, sortBy);
     }
 
-//    @PostMapping
-//    @ResponseStatus(HttpStatus.CREATED)
-//    public EventRespDTO save(@RequestBody @Validated EventDTO payload, BindingResult validationResult) {
-//        if (validationResult.hasErrors()) {
-//            throw new BadRequestException("da fare con errore guisto");
-//        } else {
-//            Event newEvent = this.eventService.save(payload);
-//            return new EventRespDTO(newEvent.getId());
-//        }
-//    }
+    //serve a gli organizer
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasAuthority('ORGANIZER')")
+    public EventRespDTO save(@RequestBody @Validated EventDTO payload, @AuthenticationPrincipal User currentUser,
+                             BindingResult validationResult) {
+        if (validationResult.hasErrors()) {
+            throw new BadRequestException("da fare con errore guisto");
+        } else {
+            Event newEvent = this.eventService.save(payload, currentUser.getId());
+            return new EventRespDTO(newEvent.getId());
+        }
+    }
+
+    //put su evento sempre organizer
+    //delete su evento di organizer
+    //get by id sempre organizer
+    //all the above solo se si e il creatore dell evento
+
+    //prenotazione da parte di user
 }
